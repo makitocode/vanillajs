55076517010bc1ddb18bd301356e660d8eb646fe
makito <makitodev@outlook.com>
2019-04-01 09:29:23 -0500

    primitive types and get typeof of each of them.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 'use strict'
 
+//#region PASSING_VALUE_BY_REFERENCE
+/**
+ * Method to test example of passing value by reference
+ */
 function PassingValueByReference(){
     console.log(':::::: Passing Value by reference ::::::')
 
@@ -27,6 +31,9 @@ function PassingValueByReference(){
     console.log('result d ->', d);
 }
 
+/**
+ * Method to test example of passing value by value
+ */
 function PassingValueByValue(){
     console.log(':::::: Passing Value by value ::::::')
 
@@ -43,7 +50,79 @@ function PassingValueByValue(){
 }
 
 
-//Test Passing value by reference - OBJECT
-PassingValueByReference();
-PassingValueByValue();
+//Run Test
+// PassingValueByReference();
+// PassingValueByValue();
+//#endregion 
+
+
+//#region PRIMITIVE_TYPES
+console.log('\n');
+console.log(':::: Primitive types');
+console.log('\n');
+/**
+ * Method to print some example of primitives values 
+ * and get the typeof ofeach of them
+ */
+function PrintPrimitiveTypes_and_get_typeOf(){
+    let a = 2;
+    console.log('let a = 2 //number');
+    let b = "two";
+    console.log('let b = "two" //string');
+    let c = true;
+    console.log('let c = true //bolean');
+    let d = null;
+    console.log('let d = null //"object" -> not exactly');
+    let e = undefined;
+    console.log('let e = undefined //undefined');
+
+    console.log('---- TypeOf ------');
+    console.log('typeof a ->', typeof a);
+    console.log('typeof b ->', typeof b);
+    console.log('typeof c ->', typeof c);
+    console.log('typeof d ->', typeof d);
+    console.log('typeof e ->', typeof e);
+}
+
+/**
+ * Method to test new primitive type symbol.
+ * Symbol introduce in ES6
+ */
+function SymbolsforFun(){
+    //Instead, just an assignment to Symbol will create a new symbol with a unique ID:
+    let sym = Symbol('unique');
+    console.log("let sym = Symbol('unique')");
+    console.log('sym -> ', sym);
+    let obj = {
+        [sym]: 1
+    }
+    console.log('obj -> ', obj);
+    console.log('-------------');
+    //The ID, however, is not the used-defined string "sym", it is created internally.
+    //This is demonstrated in the following example.
+    let x = Symbol('sym') === Symbol('sym');
+    console.log("let x = Symbol('sym') === Symbol('sym')");
+    console.log('result -> ', x);
+    console.log('-------------');
+    //Symbols can be used whenever you need unique IDs. Hence, they can also be used
+    //to create constants in enumerable lists of IDs:
+    const seasons = {
+        winter: Symbol('winter'),
+        spring: Symbol('spring'),
+        summer: Symbol('summer'),
+        autumn: Symbol('autumn')
+    }
+
+    console.log('symbol seasons ', seasons);
+}
+
+//run test region
+//  PrintPrimitiveTypes_and_get_typeOf();
+// console.log('\n');
+// SymbolsforFun();
+// console.log('\n');
+//#endregion
+
+
+
 // export { PassingValueByReference, PassingValueByValue}
